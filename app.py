be165060f788575c8525f31232c2af89e01109d7
meowpunch <jinhoonbae@gmail.com>
2023-09-05 23:53:17 +0200

    show chat history

--- app.py
+++ app.py
@@ -10,8 +10,8 @@ from langchain.vectorstores import FAISS
 from langchain.vectorstores.base import VectorStore
 from langchain.memory import ConversationBufferMemory
 from langchain.chains import ConversationalRetrievalChain
-from langchain.llms import OpenAI
 from langchain.chat_models import ChatOpenAI
+from langchain.llms import HuggingFaceHub
 
 from static.htmlTemplates import css, bot_template, user_template
 
@@ -50,7 +50,11 @@ def get_vectorstore(text_chunks: List[str]) -> VectorStore:
 
 
 def get_conversation_chain(vectorstore: VectorStore):
-    llm = ChatOpenAI()
+    # llm = ChatOpenAI()
+    llm = HuggingFaceHub(
+        repo_id="google/flan-t5-xxl",
+        model_kwargs={"temperature": 0.5, "max_length": 2048}
+    )
 
     memory = ConversationBufferMemory(
         memory_key="chat_history",
@@ -70,6 +74,15 @@ def handle_user_input(user_question: str):
     response = st.session_state.conversation(
         {'question': user_question}
     )
+    st.session_state.chat_history = response['chat_history']
+
+    for i, message in enumerate(st.session_state.chat_history):
+        if i % 2 == 0:
+            st.write(user_template.replace("{{MSG}}", message.content),
+                     unsafe_allow_html=True)
+        else:
+            st.write(bot_template.replace("{{MSG}}", message.content),
+                     unsafe_allow_html=True)
 
 
 def main():
@@ -84,16 +97,14 @@ def main():
     if "conversation" not in st.session_state:
         st.session_state["conversation"] = None
 
+    if "chat_history" not in st.session_state:
+        st.session_state["chat_history"] = None
+
     st.header("Chat with multiple PDFs :books:")
 
     user_question = st.text_input("Ask a question about your documents:")
     if user_question:
         handle_user_input(user_question)
-
-    st.write(user_template.replace("{{MSG}}", "Hello bot"),
-             unsafe_allow_html=True)
-    st.write(bot_template.replace("{{MSG}}", "Hello human"),
-             unsafe_allow_html=True)
 
     with st.sidebar:
         st.subheader("Your documents")
